e0574c78e11413c200b884667ce0aebf3f7ac43c
thezoot3 <thezoot3@gmail.com>
2024-07-12 11:49:26 +0900

    for production

--- src/lib/CloudStorage/environment.ts
+++ src/lib/CloudStorage/environment.ts
@@ -1,7 +1,9 @@
 import { Storage } from '@google-cloud/storage'
 
+// @ts-ignore
 const storage = new Storage({
     projectId: 'seda-389608',
+    keyFilename: './seda-389608-e56fb5332d8c.json',
 })
 
 export const defaultBucket = 'semimi'
--- src/lib/VertexAI/environment.ts
+++ src/lib/VertexAI/environment.ts
@@ -2,4 +2,8 @@ import { VertexAI } from '@google-cloud/vertexai'
 
 const project: string = 'seda-389608'
 const location: string = 'asia-northeast3'
-export const vertexAI = new VertexAI({ project, location })
+export const vertexAI = new VertexAI({
+    project,
+    location,
+    googleAuthOptions: { keyFilename: './seda-389608-e56fb5332d8c.json' },
+})
