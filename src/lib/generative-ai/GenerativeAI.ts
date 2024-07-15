d19c88335c7c57c444e7e31f033588fe342b1228
thezoot3 <thezoot3@gmail.com>
2024-07-15 09:15:18 +0900

    gemini harm config

--- src/lib/generative-ai/GenerativeAI.ts
+++ src/lib/generative-ai/GenerativeAI.ts
@@ -24,6 +24,22 @@ export default class GeminiFlash {
                             category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
                             threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
                         },
+                        {
+                            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+                        },
+                        {
+                            category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+                            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+                        },
+                        {
+                            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+                            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+                        },
+                        {
+                            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                            threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+                        },
                     ],
                     systemInstruction: this.instruction,
                 })
